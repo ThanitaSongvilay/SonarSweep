280c01fb337a10e9ce12d24d9be107ed27e06b35
ThanitaSongvilay <Thanita98@hotmail.com>
2022-01-09 20:04:28 +0100

    Binary diagnostics done wq

--- pythonProject/Binary Diagnostic.py
+++ pythonProject/Binary Diagnostic.py
@@ -1,22 +1,46 @@
-#bit = 12
-#fins the most common rate in tje correspondent position
-#Then multiply gamma and epsilon
-
-gammaRate = 0
-epsilonRate = 0
-
-#first take in user input in a list...
-
+gammaRate = []
+epsilonRate = []
 binarylist = []
+
 
 def inputList():
   while True:
-    line = int(input())
+    line = input()
     if line:
-      binarylist.append(line)
+      binarylist.append(list(line))
+
     else:
       break
-  print(binarylist)
+  # for obj in binarylist:
+  # print(obj)
+  return binarylist
 
 
-inputList()
+def checkRows(binarylist):
+  for c in range(12):
+    zero = 0
+    one = 0
+    for i in binarylist:
+      if i[c] == "0":
+        zero += 1
+      else:
+        one += 1
+    if zero > one:
+      gammaRate.append("0")
+      epsilonRate.append("1")
+    if one > zero:
+      gammaRate.append("1")
+      epsilonRate.append("0")
+
+  epsilon = "".join(epsilonRate)
+  gamma = "".join(epsilonRate)
+
+  e = int(epsilon, 2)
+  g = int(gamma, 2)
+
+  result = e * g
+
+  print(result)
+
+
+checkRows(inputList())
